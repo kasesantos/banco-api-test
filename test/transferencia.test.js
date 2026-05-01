83d5d090b1c67e2d2d3b58b5c69f43e8a62fa236
KASÉ\kases <kasefilho@gmail.com>
2026-05-01 20:36:56 -0300

    Adicionando o hook de beforeEach para captura do token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,13 +5,13 @@ const { obterToken } = require('../helpers/autenticacao');
 
 describe ('Transferências', () => {
     describe('POST /transferencias', () => {
+        let token
+        
         beforeEach(async () => {
-            const token = await obterToken('julio.lima', '123456')
+            token = await obterToken('julio.lima', '123456')
         })  
         
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {
-            const token = await obterToken('julio.lima', '123456')
-
             const resposta = await request('http://localhost:3000')
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
@@ -24,21 +24,10 @@ describe ('Transferências', () => {
                 })
                 
                 expect(resposta.status).to.equal(201);
-                console.log(resposta.body);
         });
                       
         it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de R$ 10,00', async () => {
-const respostaLogin = await request('http://localhost:3000')
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-                    })
-
-            const token = respostaLogin.body.token
-
-            const resposta = await request('http://localhost:3000')
+                const resposta = await request('http://localhost:3000')
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
