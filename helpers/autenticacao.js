6be0bd624ffa0e830be50b3d0a3e3708d8c2c00b
KASÉ\kases <kasefilho@gmail.com>
2026-05-01 20:13:13 -0300

    Criando um helper de autenticação para obter o Token

---
+++ helpers/autenticacao.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+
+const obterToken = async (usuario, senha) => {
+    const respostaLogin = await request('http://localhost:3000')
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({
+            'username': usuario,
+            'senha': senha
+            })
+
+    return respostaLogin.body.token
+}       
+
+module.exports = {  
+    obterToken
+}
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,18 +1,12 @@
 const request = require('supertest');
 const {expect} = require('chai')
+require('dotenv').config()
+const { obterToken } = require('../helpers/autenticacao');
 
 describe ('Transferências', () => {
     describe('POST /transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {
-            const respostaLogin = await request('http://localhost:3000')
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-                    })
-
-            const token = respostaLogin.body.token
+            const token = await obterToken('julio.lima', '123456')
 
             const resposta = await request('http://localhost:3000')
                 .post('/transferencias')
